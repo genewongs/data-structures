29aa732395825e9eb827067a9af246a86aea393b
Gene Wong <gene.h.wong@gmail.com>
2022-02-24 15:57:40 -0800

    implemented a working and passing solution for both stack and queue in functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
+
+var queueMethods = {
+  enqueue: function(val) {
+    var keys = Object.keys(this.storage);
+    var key = keys[keys.length - 1] + 1;
+    this.storage[key] = val;
+  },
+
+  dequeue: function() {
+    var keys = Object.keys(this.storage);
+    var key = keys[0];
+    var toBeDeleted = this.storage[key];
+    delete this.storage[key];
+    return toBeDeleted;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
+
+var stackMethods = {
+  push: function(val) {
+    //push adds to very end
+    //get keys
+    //get last element in keys
+    //set obj at keys as value
+    var keys = Object.keys(this.storage);
+    var key = keys.length;
+    this.storage[key] = val;
+  },
+
+  pop: function() {
+    var keys = Object.keys(this.storage);
+    var key = keys[keys.length - 1];
+    var toBeDeleted = this.storage[key];
+    delete this.storage[key];
+    return toBeDeleted;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
