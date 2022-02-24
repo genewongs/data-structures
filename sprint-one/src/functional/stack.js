5b2446c9216d339e54e739d7f3e5b7f2ae6a6dd6
Gene Wong <gene.h.wong@gmail.com>
2022-02-24 15:19:24 -0800

    Implemented working stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    var key = keys.length;
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var key = keys[keys.length - 1];
+    var toBeDeleted = storage[key];
+    delete storage[key];
+    return toBeDeleted;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
