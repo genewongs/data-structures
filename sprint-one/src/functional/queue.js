3188283c48d49387fb1cd5539f48031e70f2dcdf
Gene Wong <gene.h.wong@gmail.com>
2022-02-24 13:24:32 -0800

    Implemented Queue for functional with passing tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,22 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    var key = keys[keys.length - 1] + 1;
+    storage[key] = value;
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var targetKey = keys[0];
+    var toBeDeleted = storage[targetKey];
+    delete storage[targetKey];
+    return toBeDeleted;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
