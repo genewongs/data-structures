1162c8c2a78ae64eb8c47bd470c6db55e860d6c1
Gene Wong <gene.h.wong@gmail.com>
2022-02-25 12:31:13 -0800

    implemented some of tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,34 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = new Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+
+  }
+
+  // base case, no children:
+  if (this.children.length === 0) {
+    return;
+  }
+
+  //if children array is greater than 0
+  if (this.children.length > 0) {
+
+  }
+
 };
 
 
