9160d546ccad113bd19b413be4498c44550dfb47
Gene Wong <gene.h.wong@gmail.com>
2022-02-25 11:54:48 -0800

    implemented linkedlist with passing tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create new node variable
+    var newNode = new Node(value);
+    // check if list.head is null
+    if (list.head === null) {
+      // if null, add new node as head
+      list.head = newNode;
+    } else {
+      // if not null, add new node to tail.next
+      list.tail.next = newNode;
+    }
+    // assign new node to tail
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return;
+    }
+    var currentHead = list.head.value;
+    list.head = list.head.next;
+    return currentHead;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
